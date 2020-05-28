0296871d9da08467fc5450173e763dd657881f7b
diegoA25 <00044919@uca.edu.sv>
2020-05-28 00:25:21 -0600

    Parcial02 imagenes

--- SourceCode/DEAC_2EP/MenuPrincipalComun.cs
+++ SourceCode/DEAC_2EP/MenuPrincipalComun.cs
@@ -39,7 +39,7 @@ namespace DEAC_2EP
         private void button4_Click(object sender, EventArgs e)
         {
             tableLayoutPanel1.Controls.Remove(current);
-            current = new Pedidos();
+            current = new Productos();
             tableLayoutPanel1.Controls.Add(current, 0, 1);
             tableLayoutPanel1.SetColumnSpan(current, 4);
         }
@@ -47,7 +47,7 @@ namespace DEAC_2EP
         private void button5_Click(object sender, EventArgs e)
         {
             tableLayoutPanel1.Controls.Remove(current);
-            current = new Pedidos();
+            current = new Direccion();
             tableLayoutPanel1.Controls.Add(current, 0, 1);
             tableLayoutPanel1.SetColumnSpan(current, 4);
         }
